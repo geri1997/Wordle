247d6467ae09389b43eec6c011379f5b68f775eb
Geri <luga.geri@gmail.com>
2022-01-25 15:26:02 +0100

    Can enter letter and remove letters

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [word, setWord] = useState("sugar");
   const [guessedWords, setGuessedWords] = useState([
-    ["A", "C", "W", "Q", "E"],
+    ["", "", "", "", ""],
     ["", "", "", "", ""],
     ["", "", "", "", ""],
     ["", "", "", "", ""],
@@ -12,16 +12,36 @@ function App() {
     ["", "", "", "", ""],
   ]);
 
-  //
+  useEffect(() => {
+    function keypressHandler(e) {
+      let wordsCopy = JSON.parse(JSON.stringify(guessedWords));
+      if (e.key === "Backspace" &&wordsCopy[0]!=='') {
+        let indexOfEmpty = wordsCopy[0].findIndex((letter) => letter === "");
+        if(indexOfEmpty!==-1){wordsCopy[0][indexOfEmpty-1]=''}else{
+          wordsCopy[0][4]=''
+        }
+      }else if(wordsCopy[0].findIndex((letter) => letter === "") !== -1) {
+        wordsCopy[0][wordsCopy[0].findIndex((letter) => letter === "")] = e.key;
+      }
+      
+      setGuessedWords(wordsCopy);
+    }
+    window.addEventListener("keydown", keypressHandler);
+
+    return () => window.removeEventListener("keydown", keypressHandler);
+  }, [guessedWords]);
 
   return (
     <div className="wrapper">
-      {guessedWords.map((guessedWord) => {
-        console.log(guessedWord);
+      {guessedWords.map((guessedWord, i) => {
         return (
-          <div className="word">
-            {guessedWord.map((letter) => {
-              return <div className="letter">{letter}</div>;
+          <div key={`word${i}`} className="word">
+            {guessedWord.map((letter, ii) => {
+              return (
+                <div key={`word${i} letter${ii}`} className="letter">
+                  {letter}
+                </div>
+              );
             })}
           </div>
         );
