418ba0e4643e67ffd24ff66134f53bcdf7e54695
Geri <luga.geri@gmail.com>
2022-01-25 16:52:53 +0100

    You win message shows after you win

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   const [word, setWord] = useState("sugar");
-  const [wordIndex,setWordIndex] = useState(0);
+  const [wordIndex, setWordIndex] = useState(0);
   const [guessedWords, setGuessedWords] = useState([
     ["", "", "", "", ""],
     ["", "", "", "", ""],
@@ -13,12 +13,24 @@ function App() {
     ["", "", "", "", ""],
   ]);
 
+  function checkIfWon() {
+    if (
+      wordIndex > 0 &&
+      word[0] === guessedWords[wordIndex - 1][0] &&
+      word[1] === guessedWords[wordIndex - 1][1] &&
+      word[2] === guessedWords[wordIndex - 1][2] &&
+      word[3] === guessedWords[wordIndex - 1][3] &&
+      word[4] === guessedWords[wordIndex - 1][4]
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
   useEffect(() => {
     function keypressHandler(e) {
       let wordsCopy = JSON.parse(JSON.stringify(guessedWords));
-      
-
-
 
       if (e.key === "Backspace" && wordsCopy[wordIndex][0] !== "") {
         let indexOfEmpty = wordsCopy[wordIndex].findIndex(
@@ -37,12 +49,12 @@ function App() {
         wordsCopy[wordIndex][
           wordsCopy[wordIndex].findIndex((letter) => letter === "")
         ] = e.key;
-      }else if(e.key==='Enter'&&
-      wordsCopy[wordIndex].findIndex((letter) => letter === "") === -1){
-        setWordIndex(prevIndex=>prevIndex+1)
-
+      } else if (
+        e.key === "Enter" &&
+        wordsCopy[wordIndex].findIndex((letter) => letter === "") === -1
+      ) {
+        setWordIndex((prevIndex) => prevIndex + 1);
       }
-      
 
       setGuessedWords(wordsCopy);
     }
@@ -53,12 +65,22 @@ function App() {
 
   return (
     <div className="wrapper">
+      {checkIfWon() && <h2>You Win</h2>}
       {guessedWords.map((guessedWord, i) => {
         return (
           <div key={`word${i}`} className="word">
             {guessedWord.map((letter, ii) => {
               return (
-                <div key={`word${i} letter${ii}`} className={`letter ${wordIndex>i&&word.split('').indexOf(letter)===ii?'exact':wordIndex>i&&word.split('').includes(letter)?'incld':''}`}>
+                <div
+                  key={`word${i} letter${ii}`}
+                  className={`letter ${
+                    wordIndex > i && word.split("").indexOf(letter) === ii
+                      ? "exact"
+                      : wordIndex > i && word.split("").includes(letter)
+                      ? "incld"
+                      : ""
+                  }`}
+                >
                   {letter}
                 </div>
               );
